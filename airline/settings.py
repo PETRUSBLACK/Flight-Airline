6275b0672f275c821ad1df424a27a189f75832ec
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-01-25 12:44:11 +0100

    creating superuser

--- airline/settings.py
+++ airline/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'flights', 
+    'flights.apps.FlightsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
